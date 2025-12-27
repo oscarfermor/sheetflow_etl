8609dd2b6880844f5f2c327e977d9ed13705e751
OscarFM014 <oscar.gohan2010@hotmail.com>
2025-12-27 00:25:14 -0600

    Add logic to send .csv file to bucket per worksheet

--- src/orchestator.py
+++ src/orchestator.py
@@ -1,4 +1,4 @@
-import os
+import io
 from logger import logger
 from extractor import SheetsExtractor
 from constants import (
@@ -27,12 +27,22 @@ def main():
     )
     extractor.connect()
     data = extractor.extract_all()
-    logger.info(data)
-
+    if not data:
+        logger.error("No data extracted from Google Sheets.")
+        return
     # 2) Write raw
-    print(make_s3_key(prefix=AWS_S3_RAW_PREFIX, sheet_id=SHEET_ID, worksheet="test1"))
-    # s3Client = S3Client(bucket_name=AWS_BUCKET_NAME, region_name=AWS_REGION_NAME)
-    # write_raw(sheet_data)
+    s3Client = S3Client(bucket_name=AWS_BUCKET_NAME, region_name=AWS_REGION_NAME)
+    for worksheet, df in data.items():
+        s3_key = (
+            make_s3_key(
+                prefix=AWS_S3_RAW_PREFIX, sheet_id=SHEET_ID, worksheet=worksheet
+            )
+            + ".csv"
+        )
+        buffer = io.BytesIO()
+        df.to_csv(buffer, index=False, header=False)
+        buffer.seek(0)
+        s3Client.upload_bytes(data=buffer.getvalue(), s3_key=s3_key)
 
     print("ETL run completed!")
 
