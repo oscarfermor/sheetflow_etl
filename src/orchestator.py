f1999e4a0da093507ec62005f6e33aefafcede81
OscarFM014 <oscar.gohan2010@hotmail.com>
2025-12-26 21:00:08 -0600

    Fix: use prefix based on medallion

--- src/orchestator.py
+++ src/orchestator.py
@@ -30,7 +30,7 @@ def main():
     logger.info(data)
 
     # 2) Write raw
-    print(make_s3_key(SHEET_ID, "test1"))
+    print(make_s3_key(prefix=AWS_S3_RAW_PREFIX, sheet_id=SHEET_ID, worksheet="test1"))
     # s3Client = S3Client(bucket_name=AWS_BUCKET_NAME, region_name=AWS_REGION_NAME)
     # write_raw(sheet_data)
 
